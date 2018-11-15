f4817235f8628ad111fb9ebd34f1fa2c05542031
Hau Le <haule@hoiio.com>
2018-11-15 18:39:37 +0700

    akka actor hierarchy

---
+++ src/main/java/com/lightbend/akka/sample/PrintMyActorRefActor.java
@@ -0,0 +1,21 @@
+package com.lightbend.akka.sample;
+
+import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
+import akka.actor.Props;
+
+public class PrintMyActorRefActor extends AbstractActor {
+    static Props props() {
+        return Props.create(PrintMyActorRefActor.class, PrintMyActorRefActor::new);
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .matchEquals("printit", p -> {
+                    ActorRef secondRef = getContext().actorOf(Props.empty(), "second-actor");
+                    System.out.println("2-" + secondRef);
+                })
+                .build();
+    }
+}
