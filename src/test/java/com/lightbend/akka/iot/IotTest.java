1f2cc930f5c1776d5e4f54755ca599d511c6a254
Hau Le <haule@hoiio.com>
2018-11-15 20:22:51 +0700

    Device Actor

---
+++ src/test/java/com/lightbend/akka/iot/IotTest.java
@@ -0,0 +1,61 @@
+package com.lightbend.akka.iot;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.testkit.javadsl.TestKit;
+import com.lightbend.akka.iot.Device.ReadTemperature;
+import com.lightbend.akka.iot.Device.RespondTemperature;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+public class IotTest {
+    static ActorSystem system;
+
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        TestKit.shutdownActorSystem(system);
+        system = null;
+    }
+
+    @Test
+    public void testReplyWithEmptyReadingIfNoTemperatureIsKnown() {
+        TestKit probe = new TestKit(system);
+        ActorRef deviceActor = system.actorOf(Device.props("group", "device"));
+        deviceActor.tell(new ReadTemperature(42L), probe.getRef());
+        RespondTemperature response = probe.expectMsgClass(RespondTemperature.class);
+        assertEquals(42L, response.requestId);
+        assertEquals(Optional.empty(), response.value);
+    }
+
+    @Test
+    public void testReplyWithLatestTemperatureReading() {
+        TestKit probe = new TestKit(system);
+        ActorRef deviceActor = system.actorOf(Device.props("group", "device"));
+
+        deviceActor.tell(new Device.RecordTemperature(1L, 24.0), probe.getRef());
+        assertEquals(1L, probe.expectMsgClass(Device.TemperatureRecorded.class).requestId);
+
+        deviceActor.tell(new Device.ReadTemperature(2L), probe.getRef());
+        Device.RespondTemperature response1 = probe.expectMsgClass(Device.RespondTemperature.class);
+        assertEquals(2L, response1.requestId);
+        assertEquals(Optional.of(24.0), response1.value);
+
+        deviceActor.tell(new Device.RecordTemperature(3L, 55.0), probe.getRef());
+        assertEquals(3L, probe.expectMsgClass(Device.TemperatureRecorded.class).requestId);
+
+        deviceActor.tell(new Device.ReadTemperature(4L), probe.getRef());
+        Device.RespondTemperature response2 = probe.expectMsgClass(Device.RespondTemperature.class);
+        assertEquals(4L, response2.requestId);
+        assertEquals(Optional.of(55.0), response2.value);
+    }
+}
