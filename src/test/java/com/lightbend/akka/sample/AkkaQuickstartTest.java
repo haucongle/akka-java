194c26c856485d9e4f3510e0eddff059710eb9ca
Hau Le <haule@hoiio.com>
2018-11-16 12:04:17 +0700

    junit 5

--- src/test/java/com/lightbend/akka/sample/AkkaQuickstartTest.java
+++ src/test/java/com/lightbend/akka/sample/AkkaQuickstartTest.java
@@ -3,16 +3,18 @@ package com.lightbend.akka.sample;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.testkit.javadsl.TestKit;
+import com.lightbend.akka.iot.IotTest;
 import com.lightbend.akka.sample.Greeter.Greet;
 import com.lightbend.akka.sample.Greeter.WhoToGreet;
 import com.lightbend.akka.sample.Printer.Greeting;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AkkaQuickstartTest {
+    static final Logger LOG = Logger.getLogger(IotTest.class.getName());
     static ActorSystem system;
 
     @BeforeAll
@@ -24,6 +26,18 @@ public class AkkaQuickstartTest {
     public static void teardown() {
         TestKit.shutdownActorSystem(system);
         system = null;
+    }
+
+    @BeforeEach
+    public void beforeEachTest(TestInfo testInfo) {
+        LOG.info(() -> String.format("About to execute [%s]",
+                testInfo.getDisplayName()));
+    }
+
+    @AfterEach
+    public void afterEachTest(TestInfo testInfo) {
+        LOG.info(() -> String.format("Finished executing [%s]",
+                testInfo.getDisplayName()));
     }
 
     @Test
